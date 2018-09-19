8ff3c4302285502c480e628e8deec1b4fc063bb2
madhu-patidar <madhusudan.patidar@neosofttech.com>
2018-09-19 19:30:26 +0530

    add common use component and db json for data store

--- src/app/common/common-routing.module.ts
+++ src/app/common/common-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule, ModuleWithProviders, Component }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CalenderComponent } from './components/calender/calender.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { ImageCropperComponent } from './components/image-cropper/image-cropper.component';
+import { SelectDropdownComponent } from './components/select-dropdown/select-dropdown.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+
 
 const routes: Routes = [
 
@@ -10,7 +20,14 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
           { path: '', component: HomeComponent },
-          { path: 'home', component: HomeComponent }
+          { path: 'home', component: HomeComponent },
+          { path: 'carousel', component: CarouselComponent },
+          { path: 'calender', component: CalenderComponent },
+          { path: 'file-upload', component: FileUploadComponent },
+          { path: 'image-cropper', component: ImageCropperComponent },
+          { path: 'select-dropdown', component: SelectDropdownComponent },
+          { path: 'reactive-form', component: ReactiveFormComponent },
+          { path: 'datatable', component: DataTableComponent },
       ]
     }
   ];
